53eabb5038eb7b41e99c2c0a5d70b8db6dfcddc0
Muhammad Adnan <109526296+adnankhan344@users.noreply.github.com>
2023-08-16 12:33:48 +0500

    Update ContractController.php

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\UsernameLanguage;
+use Illuminate\Support\Facades\Session;
 
 class ContractController extends Controller
 {
@@ -14,11 +16,21 @@ class ContractController extends Controller
         $this->apiController = new \SonarSoftware\CustomerPortalFramework\Controllers\ContractController();
     }
 
-    public function index()
+    public function index($token=null)
     {
         /**
          * This is not cached, as signing a contract outside the portal cannot be detected, and so would create invalid information display here.
          */
+        if(isset($token)){
+                Session::flush();
+                $user = json_decode(base64_decode($token), true);
+                $user=(object)$user;
+                Session::put('authenticated', true);
+                Session::put('user', $user);
+                $usernameLanguage = UsernameLanguage::firstOrNew(['username' => $user->username]);
+                $usernameLanguage->language = 'US';
+                $usernameLanguage->save();
+        }
         $contracts = $this->apiController->getContracts(get_user()->account_id, 1);
         return view("pages.contracts.index", compact('contracts'));
     }
