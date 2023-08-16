1e7b76b52ed8717230dbad390f604b668f9c93bf
Muhammad Adnan <109526296+adnankhan344@users.noreply.github.com>
2023-08-16 11:24:03 +0500

    Update SonarCustomerPortalPolicy.php

--- app/Services/Csp/Policies/SonarCustomerPortalPolicy.php
+++ app/Services/Csp/Policies/SonarCustomerPortalPolicy.php
@@ -19,7 +19,7 @@ class SonarCustomerPortalPolicy extends Basic
                 'js.stripe.com',
             ])
 
-            ->addDirective(Directive::FRAME_ANCESTORS, Keyword::NONE)
+            ->addDirective(Directive::FRAME_ANCESTORS,  ['self', 'https://wonder147.codora.co.uk'])
 
             ->addDirective(Directive::FORM_ACTION, [
                 'self',
