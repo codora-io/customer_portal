182ee49fc3128fa31efd635df968567df3bcb826
Muhammad Adnan <109526296+adnankhan344@users.noreply.github.com>
2023-08-16 12:25:26 +0500

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'supports_credentials' => false,
-    'allowed_origins' => ['http://localhost:59109', 'https://ccr-demo.web.app/'],
+    'allowed_origins' => ['http://localhost:59109', 'https://ccr-demo.web.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'allowed_methods' => ['*'],
