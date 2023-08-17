a3f9b960998c7f143d17e366483255a8eb3d3eb6
Muhammad Adnan <109526296+adnankhan344@users.noreply.github.com>
2023-08-17 19:52:33 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('login', 'API\APIController@loginAPI');
 // Route::get('billing-page/{token}', 'BillingController@getBillingPage');
+Route::post('register', 'API\APIController@registerAPI');
 
